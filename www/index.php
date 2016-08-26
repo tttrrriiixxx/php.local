83b02869f03903362098ce26d103aa40ef9e7ea7
tttrrriiixxx <myicqmobail@rambler.ru>
2016-08-27 00:39:02 +0300

    Здесь пишем коммит

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<?php
+phpinfo();
+
+$x=0;
+
+print_r(getcwd());
+for ($x = 0; $x < 20; $x++) {
+    echo $x;
+    echo "<br/>";
+    
+}
+//function functionName($param) {
+//    
+//}
+//function sdfs($sdfsd) {
+//    
+//}
+//public function sdfsd($sdf);
